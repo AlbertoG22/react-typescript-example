021a79ba64bc8da72ed129cd6ad424782181f82a
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-12-13 19:11:27 -0600

    getting user input with refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { NewTodo } from './components/NewTodo';
 import { TodoList } from './components/TodoList';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <TodoList items={todos} />
+      <NewTodo />
     </>
   )
 }
