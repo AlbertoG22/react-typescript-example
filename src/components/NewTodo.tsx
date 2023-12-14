d3941d3748358255b8beb277aebae41079a38441
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-12-13 19:47:09 -0600

    adding styles

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,26 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
+import "./NewTodo.css";
 
 type NewTodoProps = {
-    onAddTodo: (todoText: string) => void;
+  onAddTodo: (todoText: string) => void;
 };
 
 export const NewTodo: React.FC<NewTodoProps> = ({ onAddTodo }) => {
-    const textInputRef = useRef<HTMLInputElement>(null);
+  const textInputRef = useRef<HTMLInputElement>(null);
 
-    const todoSubmitHandler = (event: React.FormEvent) => {
-        event.preventDefault();
-        const enteredText = textInputRef.current!.value;
-        // console.log(enteredText);
-        onAddTodo(enteredText);
-    };
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    // console.log(enteredText);
+    onAddTodo(enteredText);
+  };
 
   return (
     <form onSubmit={todoSubmitHandler}>
-        <div>
-            <label htmlFor="todo-text">Todo Text</label>
-            <input type="text" id='todo-text' ref={textInputRef}/>
-        </div>
-        <button type='submit'>Add Todo</button>
+      <div className="form-control">
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" ref={textInputRef} />
+      </div>
+      <button type="submit">Add Todo</button>
     </form>
   );
 };
